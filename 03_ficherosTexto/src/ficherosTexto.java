90e80e9fb48d622f18312c7049985bd69178d8cc
DavidUps <davidarribas.ciclos@gmail.com>
2017-10-26 13:07:38 +0200

    Split

--- 03_ficherosTexto/src/ficherosTexto.java
+++ 03_ficherosTexto/src/ficherosTexto.java
@@ -1,95 +1,91 @@
 import java.io.*;
+import java.nio.channels.CancelledKeyException;
 
 public class ficherosTexto {
 
 	public static void escribirFichero(String[] lista) {
 
-		try
-		{
-			//1.- Crear fichero.
+		try {
+			// 1.- Crear fichero.
 			File fichero = new File("ficheroNombres.txt");
 			FileWriter ficheroEscritura = new FileWriter(fichero);
-			//2.- Escribir nombres.
-			for(int i =0; i<lista.length;i++)
-			{
+			// 2.- Escribir nombres.
+			for (int i = 0; i < lista.length; i++) {
 				ficheroEscritura.write(lista[i]);
 			}
-			
-			//3.- Cerrar fichero.
+
+			// 3.- Cerrar fichero.
 			ficheroEscritura.close();
-		}
-		catch (IOException e)
-		{
+		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 	}
-	public static void vaciarTabla(String[] lista)
-	{
-		for(int i=0; i<lista.length; i++)
-		{
-			lista[i]="";
+
+	public static void vaciarTabla(String[] lista) {
+		for (int i = 0; i < lista.length; i++) {
+			lista[i] = "";
 		}
 	}
-	
-	public static void leerFichero(String[] lista, char[] nombre)
-	{
+
+	public static void leerFichero(String[] lista) {
+
+		String cadenaFichero="";
 		
-		try 
-		{
+		try {
 			File fichero = new File("ficheroNombres.txt");
 			FileReader ficheroLectura = new FileReader(fichero);
-			//2.- Leer nombres
-			//lee 4 caracteres a capon
+			// 2.- Leer nombres
+			// lee 4 caracteres a capon
+
+			int res, i = 0;
+			boolean romper = false;
 			
-			int res, i=0;
-			boolean romper=false;
+			//Leer nombres por bloques de 4 (Se puede variar)
+			char[] nombre = new char[15];
 			
+
 			res = ficheroLectura.read(nombre);
-			while(res !=-1)
-			{
-				for(int a=0; a<nombre.length;a++)
-				{
-					if(nombre[a] == '*')
-					{
-						romper = true;
-						break;
-					}
-				}
-				lista[i] = String.valueOf(nombre);
-				if(romper==true)
-					break;
-				res = ficheroLectura.read(nombre);
+			while (res != -1) {
+				
+				
+				cadenaFichero = cadenaFichero + String.valueOf(nombre);
+				res =ficheroLectura.read(nombre);
 				i++;
-			}	
+				//hacer el split
+			}
 			ficheroLectura.close();
+			String [] corte =  new String[6];
+			corte = cadenaFichero.split(";");
+			for(int g=0; g<lista.length;g++)
+			{
+				lista [g]= corte[g];			
+			}
+			
+			for (int d = 0; d < lista.length; d++) {
+				System.out.println("Posicion" + d + ":" + lista[d]);
+			}
+
+		} catch (IOException e) {
+			
 		}
-		catch (IOException e) {
-			// TODO: handle exception
-		}
-	}
 	
-	public static void imprimirTabla(String[] lista)
-	{
+	}
+
+	public static void imprimirTabla(String[] lista) {
 		System.out.println("Contenido de la tabla");
-		for(int i=0; i<lista.length;i++)
-		{
-			System.out.println("Posicion" + i +":" + lista[i]);
+		for (int i = 0; i < lista.length; i++) {
+			System.out.println("Posicion" + i + ":" + lista[i]);
 		}
 	}
-	
-	public static void main (String[] args) 
-	{
-		String [] lista = {"Pepe*", "Ana*", "Juan*"};
-		char[] nombre = new char[5];
+
+	public static void main(String[] args) {
+		String[] lista = { "Pepe;", "Ana;", "Juan" };
 		escribirFichero(lista);
 		vaciarTabla(lista);
-		imprimirTabla(lista);
-		leerFichero(lista, nombre);
-		imprimirTabla(lista);
-		System.out.println(lista[0]);
-		System.out.println(lista[1]);
-		System.out.println(lista[2]);
+		//imprimirTabla(lista);
+		leerFichero(lista);
+		//imprimirTabla(lista);
 	}
-	
+
 }
