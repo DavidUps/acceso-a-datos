b096c7c1449646e565dea4b49bc38f7e896f2700
DavidUps <davidarribas.ciclos@gmail.com>
2017-11-17 10:41:40 +0100

    Ejercicio Personas

--- Ejercicios/src/Persona.java
+++ Ejercicios/src/Persona.java
@@ -1,4 +1,47 @@
 
 public class Persona {
 
+	private String  nombre,
+					apellido;
+	
+	private int edad;
+	
+	public Persona(String n, String a, int e)
+	{
+		n = nombre;
+		a = apellido;
+		e = edad;
+	}
+	
+	public void meterNombre(String lista)
+	{
+		nombre = lista;
+	}
+	
+	public void meterApellido(String lista)
+	{
+		apellido = lista;
+	}
+	
+	public void meterEdad(String lista)
+	{
+		int e;
+		e =  Integer.parseInt(lista);
+		edad = e;
+	}
+	
+	public String getNombre()
+	{
+		return nombre;
+	}
+	
+	public String getApellido()
+	{
+		return apellido;
+	}
+	
+	public int getEdad()
+	{
+		return edad;
+	}
 }
--- Ejercicios/src/bufferReader.java
+++ Ejercicios/src/bufferReader.java
@@ -4,22 +4,82 @@ import java.io.*;
 
 public class bufferReader {
 
-	public static void escribirFichero(String []lista)
+	public static void writeFile(String []lista)
 	{
 		try {
-			File fichero = new File("fichero.txt");
-			FileReader ficherolectura = new FileReader(fichero);
-			BufferedReader ficheroBuffer = new BufferedReader(ficherolectura);
-			//escribir
-			for(int i=0;i<lista.length;i++)
+			// 1.- Crear fichero.
+			File fichero = new File("ficheroNombresBuffered.txt");
+			FileWriter ficheroEscritura = new FileWriter(fichero);
+			BufferedWriter ficheroBuffer = new BufferedWriter(ficheroEscritura);
+			// 2.- Escribir nombres.
+			for (int i = 0; i < lista.length; i++)
 			{
 				ficheroBuffer.write(lista[i]);
-				ficheroBuffer.readLine();
+				ficheroBuffer.newLine();
 			}
+			// 3.- Cerrar fichero.
+			ficheroBuffer.close();
+		} catch (IOException e) {
+		}
+	}
+	
+	public static void readFile(String [] lista)
+	{
+		try 
+		{
+			File fichero = new File("ficheroNombresBuffered.txt");
+			FileReader ficheroLectura = new FileReader(fichero);
+			BufferedReader ficheroBuffer = new BufferedReader(ficheroLectura);
+			int i = 0;
+			String linea;
+			linea = ficheroBuffer.readLine();
+			while (linea != null) 
+			{
+				lista[i] = linea;
+				i++;
+				linea = ficheroBuffer.readLine();
+			}
+			ficheroBuffer.close();
 		} catch (Exception e) {
-			// TODO: handle exception
 		}
 		
+	}
+	
+	public static void imprimirTabla(String[] lista) {
+		System.out.println("Contenido de la tabla");
+		for (int i = 0; i < lista.length; i++) {
+			System.out.println("Posicion" + i + ":" + lista[i]);
+		}
+	}
+	
+	public static void vaciarTabla(String[] lista) {
+		for (int i = 0; i < lista.length; i++)
+		{
+			lista[i] = "";
+		}
+	}
+
+	public static void meterPersonas(String[] lista, Persona [] people) {
+		int i=0;
+		int a=0;
+		
+		while(lista[i] != null)
+		{
+			people[i].meterNombre(lista[a]);
+			a++;
+			people[i].meterApellido(lista[a]);
+			a++;
+			people[i].meterEdad(lista[a]);
+			a++;
+			i++;
+		}
+	}
+	
+	public static void imprimirPersonas(Persona [] people) {
+		for(int i=0;i<people.length;i++)
+		{
+			System.out.println("Persona"+i+"\n" + "Nombre: " + people[i].getNombre() + "Apellido: " + people[i].getApellido() + "Edad: " + people[i].getEdad());;
+		}
 	}
 	
 	public static void main(String[] args) {
@@ -28,31 +88,56 @@ public class bufferReader {
 		int p;
 		String [] lista = {"David","Arribas","21","Jose","Manuel","21","Jonatan","Domingas","23"};
 		
+		Persona [] people = new Persona[5];
+		
+		Scanner sc = new Scanner(System.in);
 		
 		do
 		{
-			Scanner sc = new Scanner(System.in);
-			p = sc.nextInt();
-			
 			System.out.println( "1.- Escribir datos y guardarlos en buffer \n" +
 							    "2.- Leer datos del fichero y almacenar sobre las clases \n" +
 								"3.- Escribir por consola los datos \n" + 
-							    "4.- Vaciar la structura" +
-								"5.- Salir");
+							    "4.- Vaciar la structura \n" +
+								"5.- meterPersonas \n" +
+							    "6.- imprimirPersonas \n" +
+							    "7.- salir \n"
+								);
+			p = sc.nextInt();
 			
 			switch (p) 
 			{
 			case 1:
-				
-				
+				writeFile(lista);
+				System.out.println("Lista creada");
 				break;
 
-			default:
+			case 2:
+				readFile(lista);
+				System.out.println("Lista leida");
+				break;
+			case 3:
+				imprimirTabla(lista);
+				System.out.println("Leer lista");
+				break;
+				
+			case 4:
+				vaciarTabla(lista);
+				break;
+				
+			case 5:
+				meterPersonas(lista,people);
+				break;
+				
+			case 6:
+				imprimirPersonas(people);
+				break;
+				
+			case 7:
 				sc.close();
 				System.exit(1);
 				break;
 			}
-		}while (p != 6);
+		}while (p != 7);
 	}
 
 }
